c2fdf83d8cb22f7c97022c0b456d2952e9c8f5c1
Mansour <mohammmmmmmmed852963@gmail.com>
2024-11-03 18:23:26 +0200

    Updated the code to allow the project to run continuously until the user decides to exit.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from playsound import playsound
 from time import sleep
+import os
 
 chars_in_Morse_Code = {
     'A': '.- ',
@@ -58,22 +59,41 @@ chars_in_Morse_Code = {
     '_': '..--.- ',
 }
 
-password = input("Enter the password you want to convert into Morse Code\n")
-Morse_Code_password = ""
+os.system('cls')
 
-for char in password:
-    if(char.upper() in chars_in_Morse_Code):
-        Morse_Code_password += chars_in_Morse_Code[char.upper()].strip()
-    else:
-        print(f"This letter {char} Cann't be converted to Morse Code.")
+print("Welcome to the MORSE CODE Text Converter Project!")
+sleep(2)
 
-print(Morse_Code_password)
+program_on = True
 
-for char in Morse_Code_password:
-    if(char == '.'):
-        playsound("dot.wav")
-    elif(char == '-'):
-        playsound("dash.wav")
-    elif(char == ' '):
-        sleep(0.1)
-    sleep(0.15)
+while(program_on):
+    os.system('cls')
+    password = input("Enter the password you want to convert into Morse Code\n")
+    Morse_Code_password = ""
+
+    for char in password:
+        if(char.upper() in chars_in_Morse_Code):
+            Morse_Code_password += chars_in_Morse_Code[char.upper()].strip()
+        else:
+            Morse_Code_password += ' '
+
+    print(Morse_Code_password)
+
+    for char in Morse_Code_password:
+        if(char == '.'):
+            playsound("dot.wav")
+        elif(char == '-'):
+            playsound("dash.wav")
+        elif(char == ' '):
+            sleep(0.1)
+        sleep(0.15)
+    
+    if(input("Would you like to add another password? (Enter \'y\' or \'n\') \n").lower() == 'n'):
+        program_on = False
+
+os.system('cls')
+
+print("Thank you for using the MORSE CODE Text Converter!")
+sleep(2)
+
+os.system('cls')
